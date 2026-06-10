ed628aa30d2d22029f25af85125507dce0d248d4
izzafaris <izzafaris.it@gmail.com>
2026-06-10 12:39:00 +0530

    Arrays

---
+++ java_script/Arrays in .js
@@ -0,0 +1,8 @@
+
+var guestList = ["Ali", "Ahmed", "Sara", "Hassan"];
+var guestName= prompt("What is your name");
+if (guestList.includes(guestName)) {
+    alert("welcome")
+}else{
+    alert("Sorry,may be next time")
+}
