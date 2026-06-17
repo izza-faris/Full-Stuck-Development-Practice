062f46070500f8e6dc6acccac5eaf7fb4af03608
izzafaris <izzafaris.it@gmail.com>
2026-06-17 16:01:12 +0530

    added 3 folders for java

---
+++ My_Task/01. Java Operators and Control Flow Statements/Operator.java
@@ -0,0 +1,11 @@
+public class Operator {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 15;
+
+        int largest = (a > b) ? a : b;
+
+        System.out.println("Largest = " + largest);
+    }
+}
