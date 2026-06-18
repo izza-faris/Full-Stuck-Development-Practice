6ba044eab87ed97bc5477a00fa1943e9b5d0fa63
izzafaris <izzafaris.it@gmail.com>
2026-06-18 12:56:20 +0530

    Calculator java

---
+++ My_Task/01. Java Operators and Control Flow Statements/Calculator.java
@@ -0,0 +1,49 @@
+
+import java.util.Scanner;
+public class Calculator{
+    public static void main(String[]args){
+        Scanner izza =new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double num1 = izza.nextDouble();
+
+        System.out.println("Enter second number: ");
+        double num2 = izza.nextDouble();
+        System.out.println("Enter an operator (+, -, *, /): ");
+        char operator = izza.next().charAt(0);
+
+        double result;
+
+        switch(operator){
+            case '+':
+                result =num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if(num2 != 0){
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                }
+                else{
+                    System.out.println("Error: Division by zero is not allowed.");
+                   
+                }
+                break;
+            default:
+                System.out.println("Invalid operator! Please use +, -, *, or /.");
+
+
+        }
+
+        izza.close();
+
+
+    }
+}
