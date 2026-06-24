f8d4a5265e40d8390953d388b10e99cb7ebe02dd
izzafaris <izzafaris.it@gmail.com>
2026-06-24 20:21:16 +0530

    Task 7 Find Duplicate Element

---
+++ My_Task/05.Java Arrays & Number Logic/FindDuplicateElement.java
@@ -0,0 +1,23 @@
+public class FindDuplicateElement {
+    public static void main(String[] args){
+    
+        int [] arr = {10,20,30,40,10,20};
+
+        System.out.println("Duplicate elements are: ");
+
+        for(int i =0; i<arr.length; i++){
+            for(int j = i+1; j<arr.length; j++){
+
+                if(arr[i]==arr[j]){
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+                 
+            
+
+        }
+
+
+    }
+}
