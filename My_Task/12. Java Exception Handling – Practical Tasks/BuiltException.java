02af1288c89c91c54533e7fd537b75a94ea9cf85
izzafaris <izzafaris.it@gmail.com>
2026-06-30 14:09:18 +0530

    TASK 1

---
+++ My_Task/12. Java Exception Handling – Practical Tasks/BuiltException.java
@@ -0,0 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class BuiltException {
+
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        
+       try {
+        
+        System.out.println("Enter first number");
+        int num1 = input.nextInt();
+        System.out.println("Enter second number");
+        int num2 = input.nextInt();
+
+        int result= num1/num2;
+        System.out.println("Result = " + result);
+        
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot devide by zero");
+        }
+         catch(InputMismatchException i){
+                 System.out.println("Error: Invalid input. Please enter integers only.");
+        }
+        
+    
+        input.close();
+    }
+    
+}
