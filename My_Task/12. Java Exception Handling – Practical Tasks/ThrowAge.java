3ebcb9cba6fe95319562cf87454bc0dec0b6aee1
izzafaris <izzafaris.it@gmail.com>
2026-06-30 21:34:34 +0530

    TASK 3 Throw age

---
+++ My_Task/12. Java Exception Handling – Practical Tasks/ThrowAge.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ThrowAge {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+
+            try {
+            System.out.println("Enter Student Age");
+             int age = input.nextInt();
+        
+            if (age<18) {
+                throw new IllegalArgumentException ("Student age must be 18 or above.");
+            }
+               
+            System.out.println("Student is eligible");
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+
+        input.close();
+    }
+}
